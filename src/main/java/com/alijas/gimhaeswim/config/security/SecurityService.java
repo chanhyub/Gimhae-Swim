5ef1e2ee83111fab24c2a6dc264a0cf7dd2de5e1
kwonkyungryul <kkr0787@naver.com>
2024-07-22 14:21:50 +0900

    승인되지 않은 유저 로그인 시 거부 로직 추가

--- src/main/java/com/alijas/gimhaeswim/config/security/SecurityService.java
+++ src/main/java/com/alijas/gimhaeswim/config/security/SecurityService.java
@@ -1,5 +1,6 @@
 package com.alijas.gimhaeswim.config.security;
 
+import com.alijas.gimhaeswim.module.common.enums.ApplyStatus;
 import com.alijas.gimhaeswim.module.user.enums.UserStatus;
 import com.alijas.gimhaeswim.module.user.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,6 +26,10 @@ public class SecurityService implements UserDetailsService {
             throw new UsernameNotFoundException("탈퇴한 회원입니다.");
         } else if (user.getStatus().equals(UserStatus.INACTIVE)) {
             throw new UsernameNotFoundException("비활성화 된 계정입니다.");
+        } else if (user.getApplyStatus().equals(ApplyStatus.WAITING)) {
+            throw new UsernameNotFoundException("승인 대기중인 계정입니다.");
+        } else if (user.getApplyStatus().equals(ApplyStatus.REJECTED)) {
+            throw new UsernameNotFoundException("승인 거부된 계정입니다.");
         }
 
         return new CustomUserDetails(user);
